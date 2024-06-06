978df1464c1265a38ed15be857735af30c49c726
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-06-06 18:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function getNumbersAfterThreeSeconds() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve([1,2,3,4]);
+		},3000)
+	})
+}
+
+getNumbersAfterThreeSeconds().then((numbers) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const oddNumbers = numbers.filter(number => number % 2 === 0);
+			document.getElementById('output').textContent = oddNumbers.join(', ');
+			resolve(oddNumbers);
+		}, 1000);
+	})
+}).then((evenNumber) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const transformedNumbers = evenNumber.map(number => number * 2);
+			document.getElementById('output').textContent = transformedNumbers.join(', ');
+			resolve(transformedNumbers);
+		}, 2000)
+	}) 
+})
